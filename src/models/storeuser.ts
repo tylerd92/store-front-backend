7344fcb81ac4ec39256f7ce4f2f345d76e39c69f
tylerd92 <frostykat@protonmail.com>
2022-01-25 00:20:30 -0500

    add create method to storeuser model

--- src/models/storeuser.ts
+++ src/models/storeuser.ts
@@ -1,4 +1,8 @@
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 import Client from '../database';
+
+dotenv.config();
 
 export type StoreUser = {
   id?: number;
@@ -7,13 +11,29 @@ export type StoreUser = {
   email: string;
 };
 
+const { 
+  SALT_ROUNDS,
+  PEPPER
+} = process.env;
+
 export class StoreUserStore {
-  // will contain code to get data from the database
-  /*
-    index
-    show 
-    create
-    update
-    delete
-  */
+  async create(u: StoreUser): Promise<StoreUser> {
+    try {
+      const saltRounds = SALT_ROUNDS as string;
+      const pepper = PEPPER;
+      const conn = await Client.connect();
+
+      const sql = 'INSERT INTO storeusers (username, user_password, email) VALUES($1, $2, $3) RETURNING *';
+      const hash = bcrypt.hashSync(
+        u.password + pepper, 
+        parseInt(saltRounds)
+      ); 
+      const result = await conn.query(sql, [u.username, hash, u.email]);
+      const user = result.rows[0];
+      conn.release();
+      return user;
+    } catch(err) {
+      throw new Error(`unable create user (${u.username}): ${err}`);
+    }
+  }
 };
