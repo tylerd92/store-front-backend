83d582a47f2dc05afde35607f09929059f83681b
tylerd92 <frostykat@protonmail.com>
2022-01-30 16:38:02 -0500

    Update storeusersSpec

--- src/models/tests/storeuserSpec.ts
+++ src/models/tests/storeuserSpec.ts
@@ -3,6 +3,14 @@ import { StoreUserStore } from "../storeuser";
 const store = new StoreUserStore();
 
 describe('StoreUser Model', () => {
+  beforeAll( async () => {
+    const result = await store.create({
+      username: 'testUser2',
+      password: 'pa$$w0rD123',
+      email: 'test2@test.com'
+    });
+  })
+
   it('create method should create a user', async () => {
     const result = await store.create({
       username: 'testUser',
@@ -10,5 +18,10 @@ describe('StoreUser Model', () => {
       email: 'test@test.com'
     });
     expect(result.username).toEqual('testUser');
+  });
+
+  it('index method should get all records', async () => {
+    const result = await store.index();
+    expect(result.length).toBe(1);
   });
 });
