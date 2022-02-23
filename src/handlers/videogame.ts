b279c6a6073d477062418acc2f30d8c196f98b10
tylerd92 <frostykat@protonmail.com>
2022-02-23 14:44:16 -0500

    Add create method in videogam handler

--- src/handlers/videogame.ts
+++ src/handlers/videogame.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { VideoGame,VideoGameStore } from '../models/videogame';
+import { VideoGame, VideoGameStore } from '../models/videogame';
 
 const store = new VideoGameStore();
 
@@ -8,8 +8,33 @@ const index = async (_req: Request, res: Response) => {
   res.json(videoGames);
 }
 
+const show = async (req: Request, res: Response) => {
+  const videoGame = await store.show(req.body.id);
+  res.json(videoGame);
+}
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const videoGame: VideoGame = {
+      title: req.body.title,
+      genre: req.body.genre,
+      price: req.body.price,
+      summary: req.body.summary
+    }
+
+    const newGame = await store.create(videoGame);
+    res.json(newGame);
+
+  } catch(err) {
+    res.status(400);
+    res.json(err);
+  }
+}
+
 const videoGameRoutes = (app: express.Application) => {
   app.get('/videogames', index);
+  app.get('/videogames/:id', show);
+  app.post('/videogames', create);
 }
 
 export default videoGameRoutes;
